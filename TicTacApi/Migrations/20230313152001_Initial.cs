fb3ed06d48c0738842183d60cfdc39b461e5b15d
diniyuha <diniyuha@gmail.com>
2023-03-15 16:47:33 +0600

    add all files

---
+++ TicTacApi/Migrations/20230313152001_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicTacApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsComplete = table.Column<bool>(type: "bit", nullable: false),
+                    Field = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
---
+++ TicTacApi/Migrations/20230314065945_RenameStatusGame.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicTacApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameStatusGame : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Games",
+                newName: "StatusGame");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StatusGame",
+                table: "Games",
+                newName: "Status");
+        }
+    }
+}
